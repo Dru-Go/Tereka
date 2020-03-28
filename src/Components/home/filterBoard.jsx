20f402795dc8b89afd9166e3df71eea0631a608b
Dre <drumac2@gmail.com>
2020-03-28 07:45:23 +0300

    Removed and Replaced the filter component with MenuLink

--- src/Components/home/filterBoard.jsx
+++ src/Components/home/filterBoard.jsx
@@ -72,35 +72,6 @@ const FilterBoard = ({fil_dispatch, ori, setOri}) => {
   );
 };
 
-// eslint-disable-next-line no-unused-vars
-const Filter = ({value, dispatch}) => {
-  const handle = window.location.pathname;
-  const path = '/' + value.toLowerCase();
-
-  // TODO: Fix Multiple Re-renders
-  console.log('Path is ', handle, 'value is', path);
-
-  const styles = {
-    active:
-      'px-4 cursor-pointer hover:text-gray-800 hover:font-bold underline text-gray-800 font-bold',
-    Inactive: 'px-4 cursor-pointer hover:text-gray-800 hover:font-bold',
-  };
-
-  useEffect(() => {
-    if (handle === path) {
-      dispatch({type: value});
-    }
-  });
-
-  return (
-    <Link to={path}>
-      <div className={handle === path ? styles.active : styles.Inactive}>
-        {value}
-      </div>
-    </Link>
-  );
-};
-
 const MenuLink = ({value, to, exact, dispatch}) => {
   const styles = {
     active:
